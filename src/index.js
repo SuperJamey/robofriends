4caa787b619de6d6a327d7a45360d7480d606a18
Jamey LaMar <102421586+SuperJamey@users.noreply.github.com>
2023-01-27 13:06:36 -0600

    add search box w/functionality and a title

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import CardList from './CardList';
+import App from './App';
 import 'tachyons';
-import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <CardList robots={robots}/>
+    <App />
 );
